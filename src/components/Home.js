de6ec6d759a014086d0c8341a33199f61ee37b6a
sameermaharjan3 <sameermaharjan38@gmail.com>
2023-04-03 12:30:08 -0500

    JSON bug patch 1.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import FilterOptions from './FilterOptions';
 
 const Home = () =>{
 
-    const {data: orders, isPending, error} = useFetch('https://sameermaharjan3.github.io/hamro-ghar-db/orders/');
+    const {data: orders, isPending, error} = useFetch('https://sameermaharjan3.github.io/hamro-ghar-db/orders');
     
     return(
         <div className="home">
